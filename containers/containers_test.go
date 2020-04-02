10e2933c13910578fd2ba23ca4aaceba78ef8203
Tom Artiom Fiodorov <tom@glassnode.com>
2020-04-02 15:01:40 +0100

    make tests run

--- containers/containers_test.go
+++ containers/containers_test.go
@@ -6,11 +6,6 @@
 
 package containers
 
-import (
-	"github.com/afiodorov/countedredblacktree/utils"
-	"testing"
-)
-
 // For testing purposes
 type ContainerTest struct {
 	values []interface{}
@@ -30,26 +25,4 @@ func (container ContainerTest) Clear() {
 
 func (container ContainerTest) Values() []interface{} {
 	return container.values
-}
-
-func TestGetSortedValuesInts(t *testing.T) {
-	container := ContainerTest{}
-	container.values = []interface{}{5, 1, 3, 2, 4}
-	values := GetSortedValues(container, utils.IntComparator)
-	for i := 1; i < container.Size(); i++ {
-		if values[i-1].(int) > values[i].(int) {
-			t.Errorf("Not sorted!")
-		}
-	}
-}
-
-func TestGetSortedValuesStrings(t *testing.T) {
-	container := ContainerTest{}
-	container.values = []interface{}{"g", "a", "d", "e", "f", "c", "b"}
-	values := GetSortedValues(container, utils.StringComparator)
-	for i := 1; i < container.Size(); i++ {
-		if values[i-1].(string) > values[i].(string) {
-			t.Errorf("Not sorted!")
-		}
-	}
 }
